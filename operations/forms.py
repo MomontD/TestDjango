39015af62af62ea9b9b264a96b2674be74699315
MomontD <momontd@gmail.com>
2023-02-15 21:24:30 +0200

    Форма для заповнення депозитів і позик

---
+++ operations/forms.py
@@ -0,0 +1,69 @@
+from main.models import deposit,loan,expenses   #шмпортуємо всі моделі таблиць щоб повязати їх з флормами і БД
+from django.forms import ModelForm,TextInput,DateInput
+
+class add_depositForm(ModelForm) :
+    class Meta :
+        model  = deposit
+        fields = ['deposit_date','deposit_sum','deposit_rate','deposit_period','start_date','end_date']
+        widgets = {
+            "deposit_date" : DateInput(attrs = {
+                'class'       : 'form-control' , #стиль від bootstrap
+                'placeholder' : 'Deposit name'
+            }),
+            "deposit_sum" : TextInput(attrs = {
+                'class'       : 'form-control' , #стиль від bootstrap
+                'placeholder' : 'Add deposit sum'
+            }),
+            "deposit_rate" : TextInput(attrs = {
+                'class'       : 'form-control' , #стиль від bootstrap
+                'placeholder' : 'Add deposit rate'
+            }),
+            "deposit_period" : TextInput(attrs = {
+                'class'       : 'form-control' , #стиль від bootstrap
+                'placeholder' : 'Add deposit period (in month)'
+            }),
+            "start_date" : DateInput(attrs = {
+                'class'       : 'form-control' , #стиль від bootstrap
+                'placeholder' : 'Deposit start date'
+            }),
+            "end_date" : DateInput(attrs = {
+                'class'       : 'form-control' , #стиль від bootstrap
+                'placeholder' : 'Deposit end date'
+            })
+        }
+
+
+class add_loanForm(ModelForm) :
+    class Meta :
+        model  = loan
+        fields = ['debt_date','debt_sum','debt_rate','debt_period','start_date','end_date']
+        widgets = {
+            "debt_date" : DateInput(attrs = {
+                'class'           : 'form-control' , #стиль від bootstrap
+                'placeholder'     : 'Loan name'
+            }),
+            "debt_sum" : TextInput(attrs = {
+                'class'       : 'form-control' , #стиль від bootstrap
+                'placeholder' : 'Add loan sum'
+            }),
+            "debt_rate" : TextInput(attrs = {
+                'class'       : 'form-control' , #стиль від bootstrap
+                'placeholder' : 'Add loan rate'
+            }),
+            "debt_period" : TextInput(attrs = {
+                'class'       : 'form-control' , #стиль від bootstrap
+                'placeholder' : 'Add loan period (in month)'
+            }),
+            "start_date" : DateInput(attrs = {
+                'class'       : 'form-control' , #стиль від bootstrap
+                'placeholder' : 'Loan start date'
+            }),
+            "end_date"   : DateInput(attrs = {
+                'class'       : 'form-control' , #стиль від bootstrap
+                'placeholder' : 'Loan end date'
+            })
+        }
+
+
+
+
--- operations/urls.py
+++ operations/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.operations, name='operations'),
-    path('add_data',views.add_data, name='add_data')
+    path('add_government',views.add_government, name='add_government'),
+    path('add_deposit',views.add_deposit, name='add_deposit'),
+    path('add_loan',views.add_loan, name='add_loan')
+
 ]
